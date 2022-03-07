19cc46ce6ff07baf9b0fcd946b304a1e3aa18239
badsession <96147956+badsession@users.noreply.github.com>
2022-03-08 00:51:13 +0200

    Automatically answers yes for all questions

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,7 @@
-sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
 sudo yum update
-sudo yum install cmake dconf docker htop jre nmap nodejs npm python3-setuptools tmux
+sudo yum install cmake dconf docker htop jre nmap nodejs npm python3-setuptools tmux -y
 sudo pip3 install --user colorama requests aiohttp
 sudo yum check-update
-sudo yum update
+sudo yum update -y
 sudo echo 'export TERM=xterm-256color' >> $HOME/.bash_profile
