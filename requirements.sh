ff9e76ab99ef24404f893d7a2374ae2affb34067
badsession <96147956+badsession@users.noreply.github.com>
2022-03-09 18:40:51 +0200

    Fix "htop" not found

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,5 @@
-sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
+sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+sudo yum check-update
 sudo yum update -y
 sudo yum install cmake dconf docker htop jre nmap nodejs npm python3-setuptools tmux -y
 sudo pip3 install --user colorama requests aiohttp
