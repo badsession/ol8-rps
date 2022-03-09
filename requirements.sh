dc9e1a036e1c1af37a8f0b3239bbd084d6b38d82
badsession <96147956+badsession@users.noreply.github.com>
2022-03-09 23:18:36 +0200

    Upgrade python, pip, install dev. tools

--- requirements.sh
+++ requirements.sh
@@ -1,10 +1,33 @@
-sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
-sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
-sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
-sudo yum check-update
-sudo yum update -y
-sudo yum install cmake dconf docker htop jre nmap nodejs npm python3-setuptools tmux -y
-sudo pip3 install --user colorama requests aiohttp
-sudo yum check-update
-sudo yum update -y
+clear
 sudo echo 'export TERM=xterm-256color' >> $HOME/.bash_profile
+sudo yum groupinstall "Development Tools" -y
+clear
+sudo yum check-update
+sudo yum update -y
+clear
+sudo yum install epel-release -y
+clear
+sudo yum check-update
+sudo yum update -y
+clear
+sudo yum install wget curl cmake dconf docker htop jre nmap nodejs npm python3-setuptools tmux openssl-devel libffi-devel bzip2-devel -y
+clear
+sudo yum check-update
+sudo yum update -y
+
+# upgrade python3.x to 3.9
+clear
+sudo yum install openssl-devel libffi-devel bzip2-devel -y
+wget https://www.python.org/ftp/python/3.9.10/Python-3.9.10.tgz
+tar xvf Python-3.9.10.tgz
+cd Python-3.9*/
+./configure --enable-optimizations
+sudo make altinstall
+clear
+python3.9 --version
+pip3.9 --version
+/usr/local/bin/python3.9 -m pip install --upgrade pip
+
+# pip requirements
+clear
+sudo pip3 install --user wheel colorama requests aiohttp proxy urllib3
