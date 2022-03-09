902f90e14e9b50fd25fd83a20f485f53b824b89c
badsession <96147956+badsession@users.noreply.github.com>
2022-03-09 17:56:02 +0200

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,5 @@
 sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
-sudo yum update
+sudo yum update -y
 sudo yum install cmake dconf docker htop jre nmap nodejs npm python3-setuptools tmux -y
 sudo pip3 install --user colorama requests aiohttp
 sudo yum check-update
